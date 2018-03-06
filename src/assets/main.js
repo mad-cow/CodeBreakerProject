e982477f66252b00ea8b2b50db9ba80142524390
mad-cow <t.coehoorn@mad-cow.ch>
2018-03-06 22:57:13 +0100

    Create getResults functionπ

--- src/assets/main.js
+++ src/assets/main.js
@@ -9,13 +9,28 @@ function guess() {
     }
 
     if ( !validateInput(input.value) ) {
-        return false;
+        return;
     } else {
         attempt.value++;
     }
 }
 
-//implement new functions here
+function getResults(input) {
+    let correct = 0;
+    let html = '<div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">';
+    for ( let i = 0; i < input.length; i++ ) {
+        if ( input.charAt(i) == answer.value.charAt(i) ) {
+            html += '<span class="glyphicon glyphicon-ok"></span>';
+            correct++;
+        } else if ( answer.value.indexOf(input.charAt(i)) > -1 ) {
+            html += '<span class="glyphicon glyphicon-transfer"></span>';
+        } else {
+            html += '<span class="glyphicon glyphicon-remove"></span>';
+        }
+    }
+
+    html += '</div></div>'
+}
 
 function setHiddenFields() {
     answer.value = Math.floor(Math.random() * 9999).toString();
