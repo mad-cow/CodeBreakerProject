34b6868452e3fbc1d3049c66f2a2cc2ca1204c9c
mad-cow <t.coehoorn@mad-cow.ch>
2018-03-06 23:07:48 +0100

    Continue Play Conditionπ

--- src/assets/main.js
+++ src/assets/main.js
@@ -18,6 +18,8 @@ function guess() {
         setMessage('You Win! :)');
     } else if ( attempt.value >= 10 ) {
         setMessage('You Lose! :(');
+    } else {
+        setMessage('Incorrect, try again.');
     }
 }
 
